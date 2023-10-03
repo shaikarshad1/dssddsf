9917314e05c83d811da2ad1f78b49f1b39765b8f
shaikarshad1 <80144453+shaikarshad1@users.noreply.github.com>
2023-10-03 16:05:49 +0530

    bokka lay

---
+++ src/main/java/com/pro/devvvv/DevvvvApplication.java
@@ -0,0 +1,13 @@
+package com.pro.devvvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvvvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/devvvv/DevvvvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.devvvv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
